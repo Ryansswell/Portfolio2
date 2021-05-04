bb819dc25f5879ce1b028887376ade80abe601ba
Ryan Anderson <ryan0425@msn.com>
2021-05-04 00:05:54 -0700

    updated the README, index, scripts

---
+++ script.js
@@ -0,0 +1,11 @@
+var linkedin = document.getElementById("linkedin");
+var github = document.getElementById("github");
+
+
+linkedin.addEventListener("click", function () {
+    var hit = window.open("https://www.linkedin.com/in/ryan-anderson-52842784/");
+});
+
+github.addEventListener("click", function () {
+    var hit = window.open("https://github.com/Ryansswell");
+});
